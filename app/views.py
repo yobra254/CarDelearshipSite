8910f7e0f62c3cb155fcfbdd37d3989247d1b42e
yobra254 <38029659+yobra254@users.noreply.github.com>
2021-02-24 16:25:03 +0300

    Addition of a Filter functionality

--- app/views.py
+++ app/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from .models import Car
+from .filters import CarFilter
 
 # Create your views here.
 def index(request):
     new_cars= Car.objects.filter(category= 'New')[:6]
     used_cars= Car.objects.filter(category= 'Used')[:6]
     latest_cars = Car.objects.all().order_by('-date')[:5]
+    all = Car.objects.all()
+    myFilter = CarFilter(request.GET, queryset = all)
+    all = myFilter.qs
 
     context={
         'new_cars':new_cars,
         'used_cars':used_cars,
-        'latest_cars':latest_cars
+        'latest_cars':latest_cars,
+        'all':all,
+        'myFilter':myFilter,
     }
 
     return render(request,'index.html', context)
